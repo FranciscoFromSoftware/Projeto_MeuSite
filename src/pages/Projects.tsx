b942d10c8e089f9599abbc57c510014fc5f6027b
Francisco_H_Lomas <franciscohenriquelomas@outlook.com>
2026-04-06 00:17:04 -0300

    2

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -55,7 +55,7 @@ export const Projects: React.FC = () => {
     {
       id: 'projeto2',
       title: 'Crescimento da População do Brasil - 2001 a 2024',
-      cover: 'https://i.ibb.co/Lzsk8pyS/Aguarde-Novos-Projetos.png',
+      cover: 'https://brasilpopulacao.streamlit.app/?embed_options=dark_theme,show_toolbar,show_colored_line,show_padding',
       images: [],
       skills: ['Python', 'Streamlit'],
       description: 'Análise publicada no StreamLit, desenvolvida em Python. Dados sobre o crescimento da população do Brasil de 2001 a 2024.',
